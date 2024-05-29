8705d406d9490eeddee42de67ed0e51b6ef06fcf
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-29 17:56:38 +0200

    Problemi axios

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,5 +1,7 @@
 <script>
 import store from '../data/store.js';
+import axios from "axios";
+
 
 export default {
     name: "AppHeader",
@@ -7,36 +9,48 @@ export default {
     data() {
         return {
             searchString: "",
-
+            cardUrl: 'https://api.themoviedb.org/3/person/popular?language=it-IT&page=1',
         }
     },
     methods: {
-        getFilm() {
+        getFilmConsole() {
             console.log("cercato:", this.searchString);
         },
 
-        getMovies() {
-            const axios = require('axios');
 
-            const options = {
-                method: 'GET',
-                url: 'https://api.themoviedb.org/3/person/popular?language=en-US&page=1',
-                headers: {
-                    accept: 'application/json',
-                    Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Yjg4MDczYmU2MTY5YTE0Y2JmMjkxMTRiOGJkMzI3MCIsInN1YiI6IjY2NTcyYzkyYmZhYjg0OTg3ODU5OTcwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q6wbynsfQ1ymE7tj6s-Xy0Y6eHrhIlAdJxh3q5Uw7To'
-                }
-            };
 
-            axios
-                .request(options)
-                .then(function (response) {
-                    console.log(response.data);
-                })
-                .catch(function (error) {
-                    console.error(error);
-                });
+        // getMovies() {
+        //     const axios = require('axios');
 
-        },
+        //     const options = {
+        //         method: 'GET',
+        //         url: 'https://api.themoviedb.org/3/person/popular?language=en-US&page=1',
+        //         headers: {
+        //             accept: 'application/json',
+        //             Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI4Yjg4MDczYmU2MTY5YTE0Y2JmMjkxMTRiOGJkMzI3MCIsInN1YiI6IjY2NTcyYzkyYmZhYjg0OTg3ODU5OTcwMSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.q6wbynsfQ1ymE7tj6s-Xy0Y6eHrhIlAdJxh3q5Uw7To'
+        //         }
+        //     };
+
+        //     axios
+        //         .request(options)
+        //         .then(function (response) {
+        //             console.log(response.data);
+        //         })
+        //         .catch(function (error) {
+        //             console.error(error);
+        //         });
+
+        // },
+    },
+    created() {
+        // CHIAMATA X CARD
+        axios
+            .get(this.cardUrl)
+            .then((risultato) => {
+                this.store.movies = risultato.data.data;
+                console.log(risultato.data.result)
+
+            });
     }
 }
 </script>
@@ -50,7 +64,7 @@ export default {
                 <div class="col-1"></div>
                 <div class="col-10 d-flex justify-content-end align-items-center">
                     <input type="text" v-model="searchString" placeholder="Cosa vuoi vedere oggi?">
-                    <button @click="getMovies">SEARCH</button>
+                    <button @click="getFilmConsole">SEARCH</button>
 
                 </div>
             </div>
--- src/data/store.js
+++ src/data/store.js
@@ -2,7 +2,7 @@ import { reactive } from 'vue';
 
 const iMieiDati = reactive({
 
-
+   movies:[],
 
 });
 
