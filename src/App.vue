2e11e55db92b4493f479a78045856717a0157404
MatteoCalonaci <matteo.calonaci.1994@gmail.com>
2024-05-29 15:26:24 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import AppHeader from './components/AppHeader.vue';
+import AppMain from './components/AppMain.vue';
+// import store from './data/store.js';
+// import axios from "axios";
+</script>
+
+<template>
+  <AppHeader />
+  <AppMain />
+</template>
+
+<style scoped></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,21 @@
+<script>
+
+
+export default {
+    name: "AppMain",
+    props: {
+
+    },
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
+<template>
+    <h2>ciao</h2>
+</template>
+
+<!-- CSS -->
+<style scoped></style>
